06452165afcd4429ecce5c251bedbe0fcacba219
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-10-30 04:00:57 +0530

    feat: added better-auth instance

---
+++ client/lib/auth.ts
@@ -0,0 +1,9 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prismaClient from "./db";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prismaClient, {
+        provider: 'postgresql'
+    }),
+});
