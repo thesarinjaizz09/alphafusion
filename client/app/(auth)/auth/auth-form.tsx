1f14179dcbef4df417cf3971c776d99325cedf72
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-10-30 17:07:40 +0530

    feat: integrated the react hook form with the auth form

--- client/app/(auth)/auth/auth-form.tsx
+++ client/app/(auth)/auth/auth-form.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
@@ -6,51 +8,124 @@ import {
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import Link from "next/link"
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { redirect } from "next/navigation";
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AuthValues, AuthSchema } from "./validate";
 import { Mail, Lock, LayoutDashboard } from "lucide-react"
 
 export function AuthForm({
   className,
-  ...props
 }: React.ComponentProps<"form">) {
+  const [isPending, startTransition] = useTransition()
+
+  const form = useForm<AuthValues>({
+    resolver: zodResolver(AuthSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: AuthValues) {
+    startTransition(async () => {
+      console.log("submit data:", data);
+      // const response = await signUp.email(data);
+      // if (response.error) toast.error(response.error.message);
+      // else redirect("/");
+    });
+  }
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
-      <FieldGroup>
-        <div className="flex flex-col items-start gap-1 text-left">
-          <h1 className="text-3xl font-bold">Auth Credentials</h1>
-          <p className="text-sm w-full">
-            Enter your credentials to access the AlphaFusion Dashboard
-          </p>
-        </div>
-        <Field>
-          <FieldLabel htmlFor="email"><Mail className="size-3.5 text-primary" /> Email</FieldLabel>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </Field>
-        <Field>
-          <div className="flex items-center">
-            <FieldLabel htmlFor="password"><Lock className="size-3.5 text-primary" />Password</FieldLabel>
-            <Link
-              href="/forgot"
-              className="ml-auto text-xs underline-offset-4 hover:underline text-gray-400"
-            >
-              Forgot your password?
-            </Link>
+    <Form {...form}>
+      <form className={cn("flex flex-col gap-6", className)} onSubmit={form.handleSubmit(onSubmit)}>
+        <FieldGroup>
+          <div className="flex flex-col items-start gap-1 text-left">
+            <h1 className="text-3xl font-bold">Auth Credentials</h1>
+            <p className="text-sm w-full">
+              Enter your credentials to access the AlphaFusion Dashboard
+            </p>
           </div>
-          <Input id="password" type="password" required placeholder="********" />
-        </Field>
-        <Field>
-          <Button type="submit" className="cursor-pointer"><LayoutDashboard className="size-3" />Dashboard </Button>
-        </Field>
-        <Field>
-          <FieldDescription className="text-center text-xs">
-            Don&apos;t have credentials?{" "}
-            <Link href="/create" className="underline underline-offset-4">
-              Sign up
-            </Link>
-          </FieldDescription>
-        </Field>
-      </FieldGroup>
-    </form>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div>
+                    <FieldLabel htmlFor="email" className="mb-2">
+                      <Mail className="size-3.5 text-primary" /> Email
+                    </FieldLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="tmt@goldman.sachs"
+                      className="peer"
+                      disabled={isPending}
+                      {...field}
+                      required
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div>
+                    <div className="flex items-center">
+                      <FieldLabel htmlFor="password" className="mb-2"><Lock className="size-3.5 text-primary" />Password</FieldLabel>
+                      <Link
+                        href="/forgot"
+                        className="ml-auto text-xs underline-offset-4 hover:underline text-gray-400"
+                      >
+                        Forgot your password?
+                      </Link>
+                    </div>
+                    <Input
+                      id="password"
+                      type="password"
+                      className="pe-9"
+                      placeholder="********"
+                      disabled={isPending}
+                      {...field}
+                      required
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="cursor-pointer" disabled={isPending}><LayoutDashboard className="size-3" />Dashboard </Button>
+          <Field>
+            <FieldDescription className="text-center text-xs">
+              Don&apos;t have credentials?{" "}
+              <Link href="/create" className="underline underline-offset-4">
+                Sign up
+              </Link>
+            </FieldDescription>
+          </Field>
+        </FieldGroup>
+      </form>
+    </Form>
   )
 }
