efd14751de311b3d86a99534c16dd4991386972c
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-10-30 20:45:20 +0530

    feat: added production seo middleware

--- client/app/(auth)/create/page.tsx
+++ client/app/(auth)/create/page.tsx
@@ -1,10 +1,4 @@
-import { type Metadata } from "next"
 import { CreateForm } from "@/app/(auth)/create/create-form"
-
-export const metadata: Metadata = {
-  title: "Create - AlphaFusion",
-  description: "Sign up for AlphaFusion – the AI-powered trading ecosystem for equities, assets and many more. Get real-time forecasts, actionable signals, and autonomous trading for smarter investments."
-}
 
 export default function SignupPage() {
   return (
---
+++ client/app/(user-dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+    SidebarInset,
+    SidebarProvider,
+} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+
+export default function UserDashboardLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="[--header-height:calc(--spacing(14))]">
+            <SidebarProvider className="flex flex-col">
+                <SiteHeader />
+                <div className="flex flex-1">
+                    <AppSidebar />
+                    <SidebarInset>
+                        {children}
+                    </SidebarInset>
+                </div>
+            </SidebarProvider>
+        </div>
+    );
+}
