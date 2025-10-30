ac1aa329610c978eb1dc1b132fdc5ed853d157a2
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-10-30 17:22:00 +0530

    feat: added better-auth api routes

--- client/lib/auth-client.ts
+++ client/lib/auth-client.ts
@@ -1,5 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
-export const authClient = createAuthClient({
+const authClient = createAuthClient({
     baseURL: "http://localhost:3000"
 })
+
+export const { signUp, signIn } = authClient
